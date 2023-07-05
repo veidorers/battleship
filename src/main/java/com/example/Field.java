6fa4e33916c93badf69e03869bc2cbc80a13eb8b
Sergey Fomenko <artem.work749@gmail.com>
2023-07-05 12:59:36 +0300

    printField a bit better

--- src/main/java/com/example/Field.java
+++ src/main/java/com/example/Field.java
@@ -24,7 +24,10 @@ public class Field {
     }
 
     public void printField() {
+        System.out.println("  0️⃣1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣");
+
         for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
             for (int j = 0; j < 10; j++) {
                 System.out.print(field[i][j]);
             }
